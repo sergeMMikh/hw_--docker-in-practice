5f47103d8d54f9f85a5960f9cb3f9de61d258057
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2024-07-19 14:33:48 +0100

    added install_docker_docker-compose.sh

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
 output "application_public_ip" {
-  value       = aws_instance.nginx_srv[0].public_ip
+  value = aws_instance.nginx_srv[0].public_ip
+}
+
+output "application_instance_id" {
+  value = aws_instance.nginx_srv[0].id
+}
+
+output "application_public_dns" {
+  value = aws_instance.nginx_srv[0].public_dns
 }
